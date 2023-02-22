2c03468881847ed6e4024961a78bb1673539e58e
Zota, Iosif <iosif.zota@in-tech.com>
2023-02-22 17:16:05 +0200

    Project setup: Use catch2 via conan not via CMake Fetch.

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -1,6 +1,20 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+#include <iostream>
 
 TEST_CASE("Run command manual ruleset dir", "Run rulesets from a dir given by hand against a log file")
 {
+    std::cout << "\nTEST\n";
     REQUIRE(true);
+}
+
+int main(int argc, char *argv[])
+{
+    // your setup ...
+
+    int result = Catch::Session().run(argc, argv);
+
+    // your clean-up...
+
+    return result;
 }
